8ee7562a41c44e051f3ffd896143bfff3702af7e
elishowbr <elissonvictor.01@hotmail.com>
2025-12-08 11:01:37 -0300

    release update v2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link"
+
 export default function Footer() {
     return (
-        <footer className=" bottom-12 text-xs text-chocolate-600 font-medium w-full text-center">
-            <h2>© 2025 Ama Brownie. Feito com ❤︎</h2>
+        <footer className="bottom-12 text-xs text-chocolate-600 font-medium w-full text-center">
+            <h2>© 2025 Ama Brownie. Feito com <Link target="_" href="https://github.com/elishowbr">❤︎</Link></h2>
         </footer>
     )
 }
